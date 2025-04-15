d0dea6ff9bc1204023f6c2edfc3756b909ae75c2
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-04-15 16:41:45 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "web" {
   key_name      = var.key_name
 
   vpc_security_group_ids = [var.security_group_id]  # Attach the security group
-  user_data = file(var.EC2_USER_DATA)
+  user_data = file("${path.root}/${var.EC2_USER_DATA}")
 
   tags = {
     Name        = var.instance_name
