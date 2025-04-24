e1396322b787b4e5fd83b039cb2108e95ede6953
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-04-24 10:17:22 +0530

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "backenddb-terraform"
-    key    = "state"
-    region = "us-east-1"
+    bucket = var.remote_state_bucket
+    key    = var.remote_state_key
+    region = var.remote_state_region
     #dynamodb_table = "backend-db"
     use_lockfile = true
   }
