971a1a4306cdcd6163ae4e81ed710a310fd2fcaf
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-05-05 15:01:08 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -12,3 +12,8 @@ output "iam_user_arn" {
   description = "IAM User ARN"
   value       = aws_iam_user.user.arn
 }
+
+output "instance_profile_name" {
+  value       = aws_iam_instance_profile.ec2_profile[0].name
+  description = "The name of the EC2 instance profile"
+}
