6c1c6fa4a1df27f2e45c44d2fa1712d5bd7d113d
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-05-05 15:49:54 +0530

    Update output.tf

--- modules/route53/output.tf
+++ modules/route53/output.tf
@@ -1,9 +1,9 @@
-output "zone_id" {
-  description = "ID of the hosted zone"
-  value       = aws_route53_zone.this.zone_id
+output "record_fqdn" {
+  description = "Fully qualified domain name of the record"
+  value       = aws_route53_record.this.fqdn
 }
 
-output "record_fqdn" {
-  description = "FQDN of the record"
-  value       = aws_route53_record.this.fqdn
+output "zone_id" {
+  description = "Hosted zone ID"
+  value       = data.aws_route53_zone.this.zone_id
 }
