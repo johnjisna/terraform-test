a4b78ae40f688fdc1a286e1e463dd17a601e4b13
Jisna C John <109332601+johnjisna@users.noreply.github.com>
2025-05-05 15:48:03 +0530

    Update main.tf

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,11 +1,13 @@
-resource "aws_route53_zone" "this" {
-  name = var.zone_name
+data "aws_route53_zone" "this" {
+  name         = var.zone_name
+  private_zone = false  # Set to true if it's a private hosted zone
 }
 
 resource "aws_route53_record" "this" {
-  zone_id = aws_route53_zone.this.zone_id
+  zone_id = data.aws_route53_zone.this.zone_id
   name    = var.record_name
   type    = var.record_type
   ttl     = var.ttl
   records = var.records
 }
+
